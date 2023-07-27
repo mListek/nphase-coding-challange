25a11e652f83f67f6b2c0fbbbc19d1a6b13743fc
mListek <michal.listex@gmail.com>
2023-07-27 09:34:46 +0200

    implemented discount method

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,24 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ShoppingCartService {
 
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(product -> calculateProductPrice(product).setScale(1, RoundingMode.HALF_UP))
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    public BigDecimal calculateProductPrice(Product product) {
+        BigDecimal result = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+
+        return product.getQuantity() > 3 ? result.multiply(BigDecimal.valueOf(0.9)) : result;
     }
 }
