7aa991c9a170e6eb263c8b2500a9b3968b52e0de
mListek <michal.listex@gmail.com>
2023-07-27 09:35:09 +0200

    added new test

--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -6,7 +6,6 @@ import com.nphase.entity.ShoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
@@ -16,13 +15,25 @@ public class ShoppingCartServiceTest {
     @Test
     public void calculatesPrice()  {
         ShoppingCart cart = new ShoppingCart(Arrays.asList(
-                new Product("Tea", BigDecimal.valueOf(5.0), 2),
-                new Product("Coffee", BigDecimal.valueOf(6.5), 1)
+                new Product("Tea", BigDecimal.valueOf(5.0), 1, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 2, "drinks")
         ));
 
         BigDecimal result = service.calculateTotalPrice(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+        Assertions.assertEquals(BigDecimal.valueOf(12.0), result);
+    }
+
+    @Test
+    public void calculatesDiscountedPrice() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+            new Product("Tea", BigDecimal.valueOf(5.0), 5, "drinks"),
+            new Product("Coffee", BigDecimal.valueOf(3.5), 3, "drinks")
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        Assertions.assertEquals(BigDecimal.valueOf(33.0), result);
     }
 
 }
